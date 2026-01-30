dfbec77cf53dfd3d84f33830fc6ec59121ca882f
qi tian da sheng <noppasin.sr@gmail.com>
2026-01-31 01:21:36 +0700

    feat: add vpc

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.vpc_name
+    }
+  )
+}
